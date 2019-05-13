716a1426d95639be65d0072e32d8f7491e2d0da6
YuTengjing <ytj2713151713@gmail.com>
2019-05-13 22:10:59 +0800

    refactor: add close icon to register page

--- src/pages/common/registerPage/index.tsx
+++ src/pages/common/registerPage/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'antd';
 import { RegisterForm } from '../../../components';
 import './style.scss';
 
@@ -6,6 +8,9 @@ export const RegisterPage = () => {
     return (
         <main className="register-page">
             <div className="register-page-panel">
+                <Link className="register-page-close" to="/login">
+                    <Icon type="close" />
+                </Link>
                 <span className="register-page-welcome">欢迎注册微商城</span>
                 <RegisterForm />
             </div>
